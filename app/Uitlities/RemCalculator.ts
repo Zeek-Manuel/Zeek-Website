17ccf05b118c8912c168a9b15a53c722f729e26b
Emmanuel Quarshie <ekwesijnr@gmail.com>
2023-12-22 09:48:48 +0000

    c

--- app/Uitlities/RemCalculator.ts
+++
@@ -1,7 +0,0 @@
-"use client"
-function RemCalculator<T extends number>(unitInPixels: T): string {
-    const pixelConstant = 16;
-    return `${unitInPixels / pixelConstant}rem`;
-}
-
-export default RemCalculator;
